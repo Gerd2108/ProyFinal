c56b6aad344ad7a4a34f6f8a5d46a540033a9bf4
Gerd2108 <145696915+Gerd2108@users.noreply.github.com>
2025-10-23 17:44:15 -0500

    Actualizacion Producto v2

--- src/clases/Producto.java
+++ src/clases/Producto.java
@@ -6,4 +6,37 @@ public class Producto {
     private String nomProducto;
     private String catProducto;
 
+    public Producto() {
+    }
+
+    public Producto(int idProducto, String nomProducto, String catProducto) {
+        this.idProducto = idProducto;
+        this.nomProducto = nomProducto;
+        this.catProducto = catProducto;
+    }
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getNomProducto() {
+        return nomProducto;
+    }
+
+    public void setNomProducto(String nomProducto) {
+        this.nomProducto = nomProducto;
+    }
+
+    public String getCatProducto() {
+        return catProducto;
+    }
+
+    public void setCatProducto(String catProducto) {
+        this.catProducto = catProducto;
+    }
+
 }
