6bf458557aa1cd5bb49edfb7cd193a3e28c77184
Gerd2108 <145696915+Gerd2108@users.noreply.github.com>
2025-10-22 23:07:47 -0500

    Clases

--- src/clases/Usuario.java
+++ src/clases/Usuario.java
@@ -8,55 +8,6 @@ public class Usuario {
     private String apellido;
     private String tipoUsuario;
 
-    public Usuario() {
-    }
-
-    public Usuario(int idUsuario, String dni, String nombre, String apellido, String tipoUsuario) {
-        this.idUsuario = idUsuario;
-        this.dni = dni;
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.tipoUsuario = tipoUsuario;
-    }
-
-    public int getIdUsuario() {
-        return idUsuario;
-    }
-
-    public void setIdUsuario(int idUsuario) {
-        this.idUsuario = idUsuario;
-    }
-
-    public String getDni() {
-        return dni;
-    }
-
-    public void setDni(String dni) {
-        this.dni = dni;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public String getTipoUsuario() {
-        return tipoUsuario;
-    }
-
-    public void setTipoUsuario(String tipoUsuario) {
-        this.tipoUsuario = tipoUsuario;
-    }
+   
 
 }
