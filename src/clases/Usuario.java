081ba9c26b107f8a503d37ff56fdab31b4504691
Gerd2108 <145696915+Gerd2108@users.noreply.github.com>
2025-10-22 22:57:09 -0500

    Clases

---
+++ src/clases/Usuario.java
@@ -0,0 +1,11 @@
+package clases;
+
+public class Usuario {
+
+    private int idUsuario;
+    private String dni;
+    private String nombre;
+    private String apellido;
+    private String tipoUsuario;
+
+}
