880c3c9d2d87fda90e60487fab922fe3806c77e4
Gerd2108 <145696915+Gerd2108@users.noreply.github.com>
2025-10-24 16:21:59 -0500

    Cambio credenciales

--- src/clases/Sistema.java
+++ src/clases/Sistema.java
@@ -11,8 +11,8 @@ public class Sistema {
 
     public Sistema() {
         usuarios.add(new Usuario(1, "12345678", "María", "Pérez", "admin1", new Administrador()));
-        usuarios.add(new Usuario(2, "87654321", "Juan", "Lopez", "trabajador123", new Trabajador()));
-        usuarios.add(new Usuario(3, "11223344", "Ana", "Torres", "invitado123", new Invitado()));
+        usuarios.add(new Usuario(2, "87654321", "Juan", "Lopez", "trabajador1", new Trabajador()));
+        usuarios.add(new Usuario(3, "11223344", "Ana", "Torres", "invitado1", new Invitado()));
 
     }
 
